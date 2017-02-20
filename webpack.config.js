049a50b8e36a8f8e30e77a371046ea96e8aed676
achmiral <miral.achmed@gmail.com>
2017-02-20 23:31:27 +0700

    ADD: basic directory structure

--- webpack.config.js
+++ webpack.config.js
@@ -5,24 +5,33 @@ var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-	entry: SRC_DIR + "/app/index.js",
+	entry: SRC_DIR + "/app/index.js", // bisa menggunakan object untuk split bundle
+	
 	output: {
 		path: DIST_DIR + "/app",
 		filename: "bundle.js",
 		publicPath: "/app/"
 	},
+	
 	module: {
 		loaders: [
-			{
-				test: /\.js?/,
-				include: SRC_DIR,
-				loader: "babel-loader",
-				query: {
-					presets: ["react", "es2015", "stage-2"]
-				}
-			}
+			{ // loader for js files
+				test: /\.js$/,
+				include: SRC_DIR, // include the source file
+				exclude: /node_modules/, // exclude the node_module directory
+				loader: "babel-loader" // using babel loader
+			}//,
+			// { // loader for css files
+			// 	test: /\.css$/,
+			// 	loader: 'style!css'
+			// }
 		]
+	},
+ 
+	resolve: {
+	 extensions: [‘’, ‘.js’, ‘.jsx’]
 	}
+
 };
 
 module.exports = config;
