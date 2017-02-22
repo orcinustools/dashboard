b83f7d8ec7d107732dc78edb62de12ece127d0d5
achmiral <miral.achmed@gmail.com>
2017-02-23 01:08:25 +0700

    MODIFY: sidebar link to use Link component

--- src/app/components/aside/asideLeft/SidebarMenu.js
+++ src/app/components/aside/asideLeft/SidebarMenu.js
@@ -1,46 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class SidebarMenu extends React.Component {
 	render() {
 		return (
       <ul className="sidebar-menu">
         <li className="active">
-          <a href="overview.html">
+          <Link to="/overview">
             <i className="fa fa-dashboard"></i> <span>Overview</span>
-          </a>
+          </Link>
         </li>
         <li className="nav-header">Projects</li>
         <li>
-          <a href="stack_list.html">
+          <Link to="/stacks">
             <i className="fa fa-server"></i> <span>Stacks</span>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="service_list.html">
+          <Link to="/services">
             <i className="fa fa-cubes"></i> <span>Services</span>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="container_list.html">
+          <Link to="/containers">
             <i className="fa fa-cube"></i> <span>Containers</span>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="volume_list.html">
+          <Link to="/volumes">
             <i className="fa fa-hdd-o"></i> <span>Volumes</span>
-          </a>
+          </Link>
         </li>
         <li className="nav-header">Community</li>
         <li>
-          <a href="katalog_list.html">
+          <Link to="/catalog">
             <i className="fa fa-book"></i> <span>Catalog</span>
-          </a>
+          </Link>
         </li>
         <li className="nav-header">Settings</li>
         <li>
-          <a href="#">
+          <Link to="/settings">
             <i className="fa fa-cog"></i> <span>Cloud Settings</span>
-          </a>
+          </Link>
         </li>
       </ul>
 		);
