021af528f57aa22afadc4a0d4c17699e81bda13f
achmiral <miral.achmed@gmail.com>
2017-03-09 21:02:21 +0700

    ADD: container list item component

---
+++ src/app/components/container/ContainerListItem.js
@@ -0,0 +1,41 @@
+import React, { PropTypes } from "react";
+import { Link } from "react-router";
+
+export default class ContainerListItem extends React.Component {
+	render() {
+		return (
+      <tr>
+        <td>asfasdfasd</td>
+        <td><Link to="/containers/wp-app-1">wp-app.1</Link></td>
+        <td><span className="label label-success">RUNNING</span></td>
+        <td>
+          <i className="fa fa-tag fa-fw" aria-hidden="true"></i> orcinus/wordpress:latest
+        </td>
+        <td>
+          <ul className="list-child">
+            <li><i className="fa fa-server fa-fw" aria-hidden="true"></i> Wordpress-app</li>
+            <li><i className="fa fa-cubes fa-fw" aria-hidden="true"></i> wp-app</li>
+            <li><i className="fa fa-hdd-o fa-fw" aria-hidden="true"></i> 1 Volume(s)</li>
+          </ul>
+        </td>
+        <td>
+          <Link to="#" className="btn btn-warning btn-sm list-stack-action"><i className="fa fa-pause" aria-hidden="true"></i></Link>
+          <Link to="#" className="btn btn-info btn-sm list-stack-action"><i className="fa fa-refresh" aria-hidden="true"></i></Link>
+          <Link to="#" className="btn btn-danger btn-sm list-stack-action"><i className="fa fa-trash-o" aria-hidden="true"></i></Link>
+        
+          <div className="dropdown" style={{ display: 'inline-block' }}>
+            <button type="button" className="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+              <i className="fa fa-bars" aria-hidden="true"></i>
+            </button>
+            
+            <ul className="dropdown-menu dropdown-min">
+              <li>
+                <Link to="#">Update</Link></li>
+              <li><Link to="#">Edit</Link></li>
+            </ul>
+          </div>
+        </td>
+      </tr>
+		)
+	}
+}
--- src/app/containers/containers/ContainerListContainer.js
+++ src/app/containers/containers/ContainerListContainer.js
@@ -5,6 +5,8 @@ import React from "react";
 import { Link } from "react-router";
 import { connect } from "react-redux";
 
+import ContainerListItem from "../../components/container/ContainerListItem";
+
 import { fetchContainers } from "../../actions/containerActions";
 
 @connect((store) => {
@@ -51,70 +53,7 @@ export default class ContainerList extends React.Component {
                     </tr>
                   </thead>
                   <tbody className="table-stack-list">
-                    <tr>
-                      <td>asfasdfasd</td>
-                      <td><Link to="/containers/wp-app-1">wp-app.1</Link></td>
-                      <td><span className="label label-success">RUNNING</span></td>
-                      <td>
-                        <i className="fa fa-tag fa-fw" aria-hidden="true"></i> orcinus/wordpress:latest
-                      </td>
-                      <td>
-                        <ul className="list-child">
-                          <li><i className="fa fa-server fa-fw" aria-hidden="true"></i> Wordpress-app</li>
-                          <li><i className="fa fa-cubes fa-fw" aria-hidden="true"></i> wp-app</li>
-                          <li><i className="fa fa-hdd-o fa-fw" aria-hidden="true"></i> 1 Volume</li>
-                        </ul>
-                      </td>
-                      <td>
-                        <Link to="#" className="btn btn-warning btn-sm list-stack-action"><i className="fa fa-pause" aria-hidden="true"></i></Link>
-                        <Link to="#" className="btn btn-info btn-sm list-stack-action"><i className="fa fa-refresh" aria-hidden="true"></i></Link>
-                        <Link to="#" className="btn btn-danger btn-sm list-stack-action"><i className="fa fa-trash-o" aria-hidden="true"></i></Link>
-                      
-                        <div className="dropdown" style={{ display: 'inline-block' }}>
-                          <button type="button" className="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                            <i className="fa fa-bars" aria-hidden="true"></i>
-                          </button>
-                          
-                          <ul className="dropdown-menu dropdown-min">
-                            <li>
-                              <Link to="#">Update</Link></li>
-                            <li><Link to="#">Edit</Link></li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>asfasdfasd</td>
-                      <td><Link to="/containers/wp-db-1">wp-db.1</Link></td>
-                      <td><span className="label label-success">RUNNING</span></td>
-                      <td>
-                        <i className="fa fa-tag fa-fw" aria-hidden="true"></i> orcinus/mysql:latest
-                      </td>
-                      <td>
-                        <ul className="list-child">
-                          <li><i className="fa fa-server fa-fw" aria-hidden="true"></i> Wordpress-app</li>
-                          <li><i className="fa fa-cubes fa-fw" aria-hidden="true"></i> wp-db</li>
-                          <li><i className="fa fa-hdd-o fa-fw" aria-hidden="true"></i> 1 Volume</li>
-                        </ul>
-                      </td>
-                      <td>
-                        <Link to="#" className="btn btn-warning btn-sm list-stack-action"><i className="fa fa-pause" aria-hidden="true"></i></Link>
-                        <Link to="#" className="btn btn-info btn-sm list-stack-action"><i className="fa fa-refresh" aria-hidden="true"></i></Link>
-                        <Link to="#" className="btn btn-danger btn-sm list-stack-action"><i className="fa fa-trash-o" aria-hidden="true"></i></Link>
-                        <div className="dropdown" style={{ display: 'inline-block' }}>
-                          <button type="button" className="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                            <i className="fa fa-bars" aria-hidden="true"></i>
-                          </button>
-                          
-                          <ul className="dropdown-menu dropdown-min">
-                            <li>
-                              <Link to="#">Update</Link></li>
-                            <li><Link to="#">Edit</Link></li>
-                          </ul>
-                        </div>
-                      </td>
-                    </tr>
-    
+                    <ContainerListItem />
                   </tbody>
                 </table>
               </div>
