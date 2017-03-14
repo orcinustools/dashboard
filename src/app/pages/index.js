3cf8ec6e45ed78fde9ba26fb00fb906bc526dee3
achmiral <miral.achmed@gmail.com>
2017-03-15 03:35:15 +0700

    UPDATE: restructure volume

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -2,7 +2,7 @@ import App from "./app/App";
 import { ContainerListPage, ContainerDetailsPage } from "./containers";
 import { ServiceListPage, ServiceDetailsPage, ServiceCreatePage } from "./services";
 import { StackDetails,	StackCreate } from "./stacks";
-import { VolumeListPage, VolumeCreate, VolumeDetailsPage } from "./volumes";
+import { VolumeListPage, VolumeCreatePage, VolumeDetailsPage } from "./volumes";
 import WelcomePage from "./welcome/WelcomePage";
 import OverviewPage from "./overview/OverviewPage";
 import PageNotFoundPage from "./pagenotfound/PageNotFoundPage";
@@ -13,7 +13,7 @@ export {
 	ContainerListPage, ContainerDetailsPage,
 	ServiceListPage, ServiceDetailsPage, ServiceCreatePage,
  	StackDetails,	StackCreate,
-	VolumeListPage, VolumeCreate, VolumeDetailsPage,
+	VolumeListPage, VolumeCreatePage, VolumeDetailsPage,
 	WelcomePage,
 	CatalogPage,
 	OverviewPage,
--- src/app/pages/volumes/VolumeListPage.js
+++ src/app/pages/volumes/VolumeListPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import VolumeList from "../../containers/volumes/VolumeListContainer";
 
 export default class VolumeListPage extends React.Component {
 	render() {
@@ -14,50 +15,7 @@ export default class VolumeListPage extends React.Component {
             <li className="active">VOLUMES</li>
           </ol>
         </section>
-
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="panel">
-              <header className="panel-heading">
-                <i className="fa fa-th-list fa-fw" aria-hidden="true"></i> Volume List
-              </header>
-              <div className="panel-body table-responsive">
-
-                <table className="table table-hover">
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Name</th>
-                      <th>Size</th>
-                      <th>Type</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody className="table-stack-list">
-                    <tr>
-                      <td>asfasdfasd</td>
-                      <td><Link to="/volumes/wp-vol">wp-vol</Link></td>
-                      <td>2GB</td>
-                      <td>nfs</td>
-                      <td>
-                        <button className="btn btn-default btn-sm list-stack-action">
-                          <i className="fa fa-sliders" aria-hidden="true"></i>
-                        </button>
-                        <button className="btn btn-danger btn-sm list-stack-action">
-                          <i className="fa fa-trash" aria-hidden="true"></i>
-                        </button>
-                      </td>
-                    </tr>
-                    
-                  </tbody>
-                </table>
-              </div>
-              <div className="panel-footer">
-                <Link to="/volumes/new" className="btn btn-primary btn-block">CREATE NEW VOLUME</Link>
-              </div>
-            </div>
-          </div>
-        </div>
+        <VolumeList />
 			</div>
 		);
 	}
