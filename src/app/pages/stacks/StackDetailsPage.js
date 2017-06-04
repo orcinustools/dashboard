4049b8517f15eecee2591425c1ab06961271f8cc
achmiral <miral.achmed@gmail.com>
2017-06-04 14:51:23 +0700

    UPDATE: stack details page

--- src/app/pages/stacks/StackDetailsPage.js
+++ src/app/pages/stacks/StackDetailsPage.js
@@ -6,20 +6,7 @@ import StackDetailsContainer from "../../containers/stacks/StackDetailsContainer
 export default class StackDetailsPage extends React.Component {
 	render() {
 		return (
-			<div>
-        <section className="content-header">
-          <h1>
-            Wordpress-app
-          </h1>
-          <ol className="breadcrumb breadcrumb-sm">
-            <li><Link to="/">HOME</Link></li>
-            <li><Link to="/stacks">GROUPS</Link></li>
-            <li className="active">Wordpress-app</li>
-          </ol>
-        </section>
-
-        <StackDetailsContainer />
-       </div>
+      <StackDetailsContainer id={this.props.params.Id} />
 		);
 	}
 }
