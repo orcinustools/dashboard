28d4c84c0a35ab31acd9bf7456b6c5987f0f43a6
achmiral <miral.achmed@gmail.com>
2017-06-04 15:14:59 +0700

    UPDATE: constant

--- src/app/config/environtment.js
+++ src/app/config/environtment.js
@@ -1,2 +1,4 @@
 export const ORCINUS_API_HOST = process.env.ORCINUS_HOST 
 export const ORCINUS_API_PORT = process.env.ORCINUS_PORT
+export const OMURA_HOST = process.env.OMURA_HOST
+export const OMURA_PORT = process.env.OMURA_PORT
